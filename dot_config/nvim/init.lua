c3b2f4db6335117d2a3ccabb4862939d32d574c6
jtrrll <jacksonterrill3@gmail.com>
2024-02-01 02:00:05 -0500

    tweaked vim and neovim configs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,22 +1,29 @@
+-- General
+vim.opt.undofile = true           -- persist undo history
+
 -- Color
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true      -- enable 24-bit color
 
 -- UI
-vim.opt.number = true         -- show line numbers
-vim.opt.relativenumber = true -- show relative line numbers
-vim.opt.cursorline = true     -- highlight current line
+vim.opt.number = true             -- show line numbers
+vim.opt.relativenumber = true     -- show relative line numbers
+vim.opt.cursorline = true     	  -- highlight current line
+vim.opt.colorcolumn = { 80, 120 } -- highlight columns
+vim.opt.signcolumn = "yes"        -- always show the sign column
+vim.opt.scrolloff = 8             -- attempt to keep space above/below the cursor
 
 -- Indentation
-vim.opt.tabstop = 2           -- tab spacing
-vim.opt.softtabstop = 2       -- unify
-vim.opt.shiftwidth = 2        -- indent/outdent by 2 columns
-vim.opt.shiftround = true     -- always indent/outdent to nearest tabstop
-vim.opt.expandtab = true      -- use spaces instead of tabs
-vim.opt.smartindent = true    -- automatically insert one extra level of indentation
-vim.opt.wrap = false          -- don't wrap text
+vim.opt.tabstop = 2               -- tab spacing
+vim.opt.softtabstop = 2           -- unify
+vim.opt.shiftwidth = 2            -- indent/outdent by 2 columns
+vim.opt.shiftround = true         -- always indent/outdent to nearest tabstop
+vim.opt.expandtab = true          -- use spaces instead of tabs
+vim.opt.smartindent = true        -- automatically insert one extra level of indentation
+vim.opt.wrap = false              -- don't wrap text
 
 -- Input
-local cursorRefreshGroup = vim.api.nvim_create_augroup("cursorRefresh", {}) -- switch between block and beam cursor shapes
+-- switch between block and beam cursor shapes
+local cursorRefreshGroup = vim.api.nvim_create_augroup("cursorRefresh", {})
 vim.api.nvim_create_autocmd({"BufEnter", "InsertLeave", "CmdlineLeave"},
   {
     pattern = "*",
