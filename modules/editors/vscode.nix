66c77e227f4bb8ba1c4c58f5bb57e8b9f4736292
Jackson Terrill <jacksonterrill3@gmail.com>
2024-06-30 14:58:22 -0400

    feat: add language-specific vscode extensions

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -7,13 +7,23 @@
     enable = true;
     extensions = with vscode-extensions.vscode-marketplace; [
       geequlim.godot-tools
+      gleam.gleam
       golang.go
+      graphql.vscode-graphql
+      graphql.vscode-graphql-syntax
       jnoortheen.nix-ide
+      ms-dotnettools.csharp
       ms-pyright.pyright
       ms-python.python
       ms-toolsai.jupyter
+      ms-vscode.cpptools
+      redhat.java
+      redhat.vscode-xml
+      redhat.vscode-yaml
+      rust-lang.rust-analyzer
       shopify.ruby-lsp
       sorbet.sorbet-vscode-extension
+      ziglang.vscode-zig
     ];
     package = pkgs.vscodium;
     userSettings = {
@@ -25,6 +35,7 @@
         enableLanguageServer = true;
         serverPath = "nil";
       };
+      redhat.telemetry.enabled = false;
       window.zoomLevel = 2;
       workbench.sideBar.location = "right";
     };
