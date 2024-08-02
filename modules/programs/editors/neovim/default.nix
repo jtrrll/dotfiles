d85a227620eed88568dea6b33f7015d5bcfce98f
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2024-08-02 02:23:44 -0400

    feat: convert activation script to python (#18)

--- modules/programs/editors/neovim/default.nix
+++ modules/programs/editors/neovim/default.nix
@@ -10,6 +10,28 @@
     enable = true;
     colorschemes.vscode.enable = true;
     extraConfigLua = ''
+      -- UI
+      -- Define a border style
+      local border = {
+        {"╭", "FloatBorder"},
+        {"─", "FloatBorder"},
+        {"╮", "FloatBorder"},
+        {"│", "FloatBorder"},
+        {"╯", "FloatBorder"},
+        {"─", "FloatBorder"},
+        {"╰", "FloatBorder"},
+        {"│", "FloatBorder"},
+      }
+
+      -- Customize the LSP hover handler
+      vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })  -- Use the same background as the editor
+      vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })  -- Use the same background as the editor
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = border
+        }
+      )
+
       -- Input
       -- switch between block and beam cursor shapes
       local cursorRefreshGroup = vim.api.nvim_create_augroup("cursorRefresh", {})
