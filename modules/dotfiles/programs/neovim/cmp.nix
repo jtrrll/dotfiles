aeed29961afd1864660a575948c2cc535d43ff9b
Jackson Terrill <jacksonterrill3@gmail.com>
2024-10-01 18:03:42 -0400

    fix: remove conflicting neovim and zellij keybinds

--- modules/dotfiles/programs/neovim/cmp.nix
+++ modules/dotfiles/programs/neovim/cmp.nix
@@ -8,8 +8,16 @@
       cmp = {
         enable = true;
         settings = {
+          completion = {
+            completeopt = "menu,menuone,noinsert";
+          };
           experimental = {
             ghost_text = {hl_group = "NonText";};
+          };
+          mapping = {
+            "<C-e>" = "cmp.mapping.close()";
+            "<S-Tab>" = "cmp.mapping.select_next_item()";
+            "<Tab>" = "cmp.mapping.confirm({ select = true })";
           };
           sources = [
             {name = "nvim_lsp";}
