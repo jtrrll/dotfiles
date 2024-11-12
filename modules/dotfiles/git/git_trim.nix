853a40180022a3c136ce1b15d4414cb1afabd5ae
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2024-11-12 15:19:23 -0500

    feat: add git-trim script

---
+++ modules/dotfiles/git/git_trim.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf config.dotfiles.git.enable {
+    home.packages = [
+      (pkgs.writeShellApplication
+        {
+          name = "git-trim";
+          runtimeInputs = [
+            pkgs.coreutils
+            pkgs.git
+            pkgs.gnugrep
+            pkgs.gum
+          ];
+          text = ''
+            if [[ "$#" -ne 1 ]]; then
+              echo "Usage: $(basename "$0") <main-branch>"
+              exit 1
+            fi
+            main_branch="$1"
+
+            gum confirm
+
+            git switch "$main_branch"
+
+            git pull origin "$main_branch"
+
+            for branch in $(git branch | grep -vE "^\*| $main_branch"); do
+                git branch -D "$branch"
+            done
+          '';
+        })
+    ];
+  };
+}
--- modules/dotfiles/screensavers/matrix.nix
+++ modules/dotfiles/screensavers/matrix.nix
@@ -6,21 +6,26 @@
 }: {
   config = lib.mkIf config.dotfiles.screensavers.enable {
     home.packages = [
-      (pkgs.writeShellScriptBin "matrix" ''
-        if [[ "$#" -ne 0 ]]; then
-          echo "Usage: $(basename "$0")"
-          exit 1
-        fi
+      (pkgs.writeShellApplication
+        {
+          name = "matrix";
+          runtimeInputs = [pkgs.coreutils pkgs.neo];
+          text = ''
+            if [[ "$#" -ne 0 ]]; then
+              echo "Usage: $(basename "$0")"
+              exit 1
+            fi
 
-        ${pkgs.neo}/bin/neo \
-          --async \
-          --color green3 \
-          --colormode 16 \
-          --defaultbg \
-          --fps 10 \
-          --lingerms 1,1000 \
-          --maxdpc 1
-      '')
+            neo \
+              --async \
+              --color green3 \
+              --colormode 16 \
+              --defaultbg \
+              --fps 10 \
+              --lingerms 1,1000 \
+              --maxdpc 1
+          '';
+        })
     ];
   };
 }
