2a7c7e70cba6fabf719fd9d8c156bafdcdf1a8df
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2024-12-21 13:22:08 -0500

    feat: add file fuzzy finding and which-key

---
+++ modules/dotfiles/editors/neovim/keymaps.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  config = lib.mkIf config.dotfiles.editors.enable {
+    programs.nixvim = {
+      globals = {
+        mapleader = " ";
+        maplocalleader = "  ";
+      };
+      plugins.which-key = {
+        enable = true;
+        settings = {
+          keys = {
+            scroll-down = "<Down>";
+            scroll-up = "<Up>";
+          };
+          preset = "modern";
+        };
+      };
+    };
+  };
+}
