655b6fbe37ed3f6e99775f3e1808423be4c00ece
Jackson Terrill <jacksonterrill3@gmail.com>
2025-03-01 13:45:25 -0500

    update deprecated vscode settings

--- modules/dotfiles/editors/vscode.nix
+++ modules/dotfiles/editors/vscode.nix
@@ -7,46 +7,48 @@
   config = lib.mkIf config.dotfiles.editors.enable {
     programs.vscode = {
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        astro-build.astro-vscode
-        biomejs.biome
-        bradlc.vscode-tailwindcss
-        evzen-wybitul.magic-racket
-        geequlim.godot-tools
-        gleam.gleam
-        golang.go
-        graphql.vscode-graphql
-        graphql.vscode-graphql-syntax
-        jnoortheen.nix-ide
-        ms-dotnettools.csharp
-        ms-pyright.pyright
-        ms-python.python
-        ms-toolsai.jupyter
-        redhat.java
-        redhat.vscode-xml
-        redhat.vscode-yaml
-        rust-lang.rust-analyzer
-        shopify.ruby-lsp
-        sorbet.sorbet-vscode-extension
-        sumneko.lua
-        vscodevim.vim
-        xadillax.viml
-        ziglang.vscode-zig
-      ];
       package = pkgs.vscodium;
-      userSettings = {
-        editor = {
-          minimap.enabled = false;
-          tabSize = config.dotfiles.editors.indentWidth;
-          rulers = config.dotfiles.editors.lineLengthRulers;
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          astro-build.astro-vscode
+          biomejs.biome
+          bradlc.vscode-tailwindcss
+          evzen-wybitul.magic-racket
+          geequlim.godot-tools
+          gleam.gleam
+          golang.go
+          graphql.vscode-graphql
+          graphql.vscode-graphql-syntax
+          jnoortheen.nix-ide
+          ms-dotnettools.csharp
+          ms-pyright.pyright
+          ms-python.python
+          ms-toolsai.jupyter
+          redhat.java
+          redhat.vscode-xml
+          redhat.vscode-yaml
+          rust-lang.rust-analyzer
+          shopify.ruby-lsp
+          sorbet.sorbet-vscode-extension
+          sumneko.lua
+          vscodevim.vim
+          xadillax.viml
+          ziglang.vscode-zig
+        ];
+        userSettings = {
+          editor = {
+            minimap.enabled = false;
+            tabSize = config.dotfiles.editors.indentWidth;
+            rulers = config.dotfiles.editors.lineLengthRulers;
+          };
+          nix = {
+            enableLanguageServer = true;
+            serverPath = "nil";
+          };
+          redhat.telemetry.enabled = false;
+          window.zoomLevel = 2;
+          workbench.sideBar.location = "right";
         };
-        nix = {
-          enableLanguageServer = true;
-          serverPath = "nil";
-        };
-        redhat.telemetry.enabled = false;
-        window.zoomLevel = 2;
-        workbench.sideBar.location = "right";
       };
     };
   };
