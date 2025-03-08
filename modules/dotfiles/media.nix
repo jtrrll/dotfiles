3cf964176cc153eeb3c0168591413f130c1e7dcc
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2025-03-08 00:59:04 -0500

    feat: start a media config

---
+++ modules/dotfiles/media.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf config.dotfiles.media.enable {
+    home.packages =
+      if pkgs.stdenv.isDarwin
+      then []
+      else [pkgs.vlc];
+    programs.beets = {
+      enable = true;
+      package = pkgs.beets.override {
+        pluginOverrides = {
+          embedart.enable = true;
+          fetchart.enabled = true;
+          lyrics.enabled = true;
+          thumbnails.enabled = true;
+        };
+      };
+      settings = {
+        directory = "~/music";
+        import = {
+          copy = false;
+          move = false;
+        };
+        library = "~/music/library.db";
+        plugins = ["embedart" "fetchart" "lyrics" "thumbnails"];
+      };
+    };
+  };
+
+  options.dotfiles.media = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Whether to enable the media configuration.";
+      example = false;
+      type = lib.types.bool;
+    };
+  };
+}
