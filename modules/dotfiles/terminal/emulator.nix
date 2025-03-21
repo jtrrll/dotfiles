0a06b23c6bd70131623ad27addb10756b406618a
Jackson Terrill <jacksonterrill3@gmail.com>
2025-03-21 03:05:27 -0400

    feat: create ghostty config

--- modules/dotfiles/terminal/emulator.nix
+++ modules/dotfiles/terminal/emulator.nix
@@ -4,20 +4,17 @@
   ...
 }: {
   config = lib.mkIf config.dotfiles.terminal.enable {
-    programs.alacritty = {
-      enable = true;
-      settings = {
-        cursor = {
-          blink_interval = 500;
-          style.blinking = "Always";
-          unfocused_hollow = true;
+    programs = {
+      alacritty = {
+        enable = true;
+        settings = {
+          env.SHELL = "${config.programs.fish.package}/bin/fish";
+          terminal.shell.program = "${config.programs.zellij.package}/bin/zellij";
         };
-        env.SHELL = "${config.programs.fish.package}/bin/fish";
-        terminal.shell.program = "${config.programs.zellij.package}/bin/zellij";
-        window.padding = {
-          x = 8;
-          y = 8;
-        };
+      };
+      ghostty = {
+        enable = true;
+        settings.command = "SHELL=${config.programs.fish.package}/bin/fish ${config.programs.zellij.package}/bin/zellij";
       };
     };
   };
--- modules/dotfiles/theme/alacritty.nix
+++ modules/dotfiles/theme/alacritty.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: {
-  config = lib.mkIf (config.dotfiles.theme.enable && config.dotfiles.theme.classicCode) {
+  config = lib.mkIf config.dotfiles.theme.enable {
     programs.alacritty.settings = {
       colors = with constants.COLOR; {
         bright = {
@@ -33,9 +33,21 @@
           bright_foreground = WHITE;
         };
       };
+      cursor = {
+        blink_interval = 500;
+        style = {
+          blinking = "Always";
+          shape = "Beam";
+        };
+        unfocused_hollow = true;
+      };
       font.normal = {
         family = "Hack Nerd Font Mono";
         style = "Regular";
+      };
+      window.padding = {
+        x = 8;
+        y = 8;
       };
     };
     stylix.targets.alacritty.enable = false;
