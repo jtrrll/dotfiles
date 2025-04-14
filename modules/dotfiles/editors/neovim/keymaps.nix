4dea83f4ab652800dc27fb80a1a4796d52d007e7
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2025-04-13 21:09:00 -0400

    fix: apply neovim navigation remap to all buffers (#97)

--- modules/dotfiles/editors/neovim/keymaps.nix
+++ modules/dotfiles/editors/neovim/keymaps.nix
@@ -9,7 +9,22 @@
         local snacks = require("snacks")
         local arrow_key_remap_enabled = false
 
-        ---@param opts? snacks.toggle.Config
+        local function apply_arrow_key_remaps(buffer)
+          buffer = buffer or 0
+          if arrow_key_remap_enabled then
+            local opts = { buffer = buffer, silent = true }
+            vim.keymap.set('n', 'n', '<Left>', opts)
+            vim.keymap.set('n', 'e', '<Down>', opts)
+            vim.keymap.set('n', 'i', '<Up>', opts)
+            vim.keymap.set('n', 'o', '<Right>', opts)
+          else
+            pcall(vim.keymap.del, 'n', 'n', { buffer = buffer })
+            pcall(vim.keymap.del, 'n', 'e', { buffer = buffer })
+            pcall(vim.keymap.del, 'n', 'i', { buffer = buffer })
+            pcall(vim.keymap.del, 'n', 'o', { buffer = buffer })
+          end
+        end
+
         function snacks.toggle.arrow_key_remap(opts)
           return snacks.toggle.new({
             id = "arrow_key_remap",
@@ -18,22 +33,17 @@
               return arrow_key_remap_enabled
             end,
             set = function(state)
-              arrow_key_remap_enabled = state or false
-
-              if arrow_key_remap_enabled then
-                vim.keymap.set('n', 'n', '<Left>')
-                vim.keymap.set('n', 'e', '<Down>')
-                vim.keymap.set('n', 'i', '<Up>')
-                vim.keymap.set('n', 'o', '<Right>')
-              else
-                vim.keymap.del('n', 'n')
-                vim.keymap.del('n', 'e')
-                vim.keymap.del('n', 'i')
-                vim.keymap.del('n', 'o')
-              end
+              arrow_key_remap_enabled = not not state
+              apply_arrow_key_remaps()
             end,
           }, opts)
         end
+
+        vim.api.nvim_create_autocmd("BufEnter", {
+          callback = function(args)
+            apply_arrow_key_remaps(args.buf)
+          end,
+        })
       '';
       globals = {
         mapleader = " ";
