9b61fd04eaa1bbe0fd8658c78fec18e5b77d2056
Jackson Terrill <jacksonterrill3@gmail.com>
2025-08-10 19:53:03 -0400

    fix checks

--- modules/checks.nix
+++ modules/checks.nix
@@ -6,24 +6,28 @@
 {
   perSystem =
     {
-      config,
       pkgs,
       system,
       ...
     }:
     {
       checks = {
-        nix-lint = pkgs.runCommandLocal "nix-lint" { } ''
-          ${config.formatter}/bin/* --check ${self}/**
-          touch $out
-        '';
+        nix-lint =
+          pkgs.runCommandLocal "nix-lint"
+            {
+              buildInputs = [ pkgs.nixfmt ];
+            }
+            ''
+              find ${self} -type f -name '*.nix' -exec nixfmt --check {} +
+              touch $out
+            '';
         snekcheck =
           pkgs.runCommandLocal "snekcheck"
             {
               buildInputs = [ inputs.snekcheck.packages.${system}.default ];
             }
             ''
-              find ${self}/** -exec snekcheck {} +
+              find ${self} -exec snekcheck {} +
               touch $out
             '';
       };
