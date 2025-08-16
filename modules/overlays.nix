6f5ddbd3ca0d7994492135fe2417c5f9e29e1486
Jackson Terrill <jacksonterrill3@gmail.com>
2025-08-16 19:45:07 -0400

    formatting

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -10,11 +10,7 @@
     in
     {
       inherit (unfreePkgs) vscode;
-      ghostty =
-        if final.stdenv.isDarwin then
-          prev.ghostty-bin
-        else
-          prev.ghostty;
+      ghostty = if final.stdenv.isDarwin then prev.ghostty-bin else prev.ghostty;
       nix-vscode-extensions = inputs.nix-vscode-extensions.extensions.${final.system};
       snekcheck = inputs.snekcheck.packages.${final.system}.default;
       unfree-vscode-extensions = unfreePkgs.vscode-extensions;
