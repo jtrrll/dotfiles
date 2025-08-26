03dcd775a16952bb9519de1d5a8b90c3e58e6ab4
Jackson Terrill <jacksonterrill3@gmail.com>
2025-08-26 17:57:19 -0400

    change configuration definitions

--- nix/home_configurations.nix
+++ nix/home_configurations.nix
@@ -17,40 +17,38 @@
         inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = (builtins.attrValues config.flake.homeModules) ++ [
-            (
-              {
-                home = {
-                  homeDirectory = HOME;
-                  stateVersion = "23.11";
-                  username = USER;
+            {
+              home = {
+                homeDirectory = HOME;
+                stateVersion = "23.11";
+                username = USER;
+              };
+              jtrrllDotfiles = {
+                bat.enable = true;
+                browsers.brave.enable = true;
+                codeDirectory.enable = true;
+                editors = {
+                  neovim.enable = true;
+                  vscode.enable = true;
                 };
-                jtrrllDotfiles = {
-                  bat.enable = true;
-                  browsers.brave.enable = true;
-                  codeDirectory.enable = true;
-                  editors = {
-                    neovim.enable = true;
-                    vscode.enable = true;
-                  };
-                  fileSystem.enable = true;
-                  gaming.enable = true;
-                  git.enable = true;
-                  homeManager.enable = true;
-                  mediaPlayback.enable = true;
-                  musicLibrary.enable = true;
-                  nix.enable = true;
-                  repeat.enable = true;
-                  screensavers.enable = true;
-                  systemInfo.enable = true;
-                  terminal.enable = true;
-                  theme = {
-                    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
-                    enable = true;
-                  };
+                fileSystem.enable = true;
+                gaming.enable = true;
+                git.enable = true;
+                homeManager.enable = true;
+                mediaPlayback.enable = true;
+                musicLibrary.enable = true;
+                nix.enable = true;
+                repeat.enable = true;
+                screensavers.enable = true;
+                systemInfo.enable = true;
+                terminal.enable = true;
+                theme = {
+                  base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
+                  enable = true;
                 };
               }
-              // cfg
-            )
+              // cfg;
+            }
           ];
         };
       pkgs = import inputs.nixpkgs {
@@ -61,13 +59,11 @@
     {
       default = mkConfig { };
       headless = mkConfig {
-        jtrrllDotfiles = {
-          browsers.brave.enable = false;
-          gaming.enable = false;
-          screensavers.enable = false;
-          theme.enable = false;
-        };
+        browsers.brave.enable = false;
+        gaming.enable = false;
+        screensavers.enable = false;
+        theme.enable = false;
       };
-      work = mkConfig { jtrrllDotfiles.gaming.enable = false; };
+      work = mkConfig { gaming.enable = false; };
     };
 }
