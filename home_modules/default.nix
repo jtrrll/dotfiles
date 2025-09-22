db341a1afbd4294ec7ec0f94300c43b2a610e3bc
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2025-09-22 10:51:19 -0400

    feat: start building nixos configurations

---
+++ home_modules/default.nix
@@ -0,0 +1,35 @@
+{ inputs, self, ... }:
+{
+  imports = [ inputs.home-manager.flakeModules.home-manager ];
+  flake.homeModules = builtins.addErrorContext "while defining Home Manager modules" {
+    default = {
+      imports =
+        let
+          constants = import ./constants.nix;
+          lib' = self.lib;
+        in
+        [
+          ./bat
+          ./browsers
+          ./code_directory
+          (import ./editors {
+            inherit (inputs.nixvim.homeModules) nixvim;
+            inherit lib';
+          })
+          ./file_system
+          (import ./gaming { inherit lib'; })
+          ./git
+          ./home_directory
+          ./home_manager
+          (import ./media_playback { inherit lib'; })
+          ./music_library
+          ./nix
+          ./repeat
+          ./screensavers
+          ./system_info
+          (import ./terminal { inherit constants; })
+          (import ./theme { inherit (inputs.stylix.homeModules) stylix; })
+        ];
+    };
+  };
+}
