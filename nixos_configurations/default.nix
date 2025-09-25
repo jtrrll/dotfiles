07d0600c1b974b63a0ecd0e3affd56a953fa00f1
Jackson Terrill <jacksonterrill3@gmail.com>
2025-09-25 00:02:18 -0400

    feat: add desktop config

--- nixos_configurations/default.nix
+++ nixos_configurations/default.nix
@@ -6,6 +6,10 @@
 }:
 {
   flake.nixosConfigurations = builtins.addErrorContext "while defining NixOS configurations" {
+    ares = lib.makeOverridable (import ./ares) {
+      inherit (inputs.nixpkgs.lib) nixosSystem;
+      dotfiles = self.nixosModules.default;
+    };
     athena = lib.makeOverridable (import ./athena) {
       inherit (inputs.nixos-hardware.nixosModules) lenovo-thinkpad-x1;
       inherit (inputs.nixpkgs.lib) nixosSystem;
