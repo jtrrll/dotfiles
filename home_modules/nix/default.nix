f0c8d5be0711f370685dede0209c0c1152d6f9f4
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2025-12-08 23:07:59 +0000

    fix: get home-manager working again on macos (#172)

--- home_modules/nix/default.nix
+++ home_modules/nix/default.nix
@@ -10,7 +10,7 @@
       enable = true;
       clean.enable = true;
     };
-    nix = {
+    nix = lib.mkIf (!pkgs.stdenv.isDarwin) {
       package = lib.mkDefault pkgs.nix;
       settings.extra-experimental-features = "flakes nix-command no-url-literals";
     };
