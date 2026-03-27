9ef59f1da86406dec5c2268b58685fafab39be35
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2026-03-27 11:03:10 -0400

    feat: add determinate nix module (#2)

--- modules/features/nix.nix
+++ modules/features/nix.nix
@@ -8,13 +8,15 @@
         "flakes"
         "nix-command"
       ];
+      substituters = [
+        "https://devenv.cachix.org"
+        "https://install.determinate.systems"
+        "https://nix-community.cachix.org"
+      ];
       trusted-public-keys = [
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      trusted-substituters = [
-        "https://devenv.cachix.org"
-        "https://nix-community.cachix.org"
       ];
     };
   };
