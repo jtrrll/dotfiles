34d089d237fd0958a7035d0c034d9c49aae49273
jtrrll <jacksonterrill3@gmail.com>
2026-05-26 14:51:58 -0400

    reorder extrasubstituters

--- modules/nixos_modules/by_name/nix/default.nix
+++ modules/nixos_modules/by_name/nix/default.nix
@@ -4,15 +4,15 @@
       "flakes"
       "nix-command"
     ];
-    substituters = [
+    extra-substituters = [
+      "https://nix-community.cachix.org"
       "https://devenv.cachix.org"
       "https://install.determinate.systems"
-      "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
