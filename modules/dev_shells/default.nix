8e9017ecc26bf19071db4edabf2cc8596475d122
jtrrll <jacksonterrill3@gmail.com>
2026-06-03 13:45:23 -0400

    remove containers from checks

--- modules/dev_shells/default.nix
+++ modules/dev_shells/default.nix
@@ -1,63 +1,72 @@
 {
   config = {
-    perSystem = _: {
-      config.devenv.shells.default =
-        { lib, pkgs, ... }:
-        {
-          enterShell = lib.concatStringsSep "\n" [
-            (lib.getExe pkgs.splash)
-            ''printf "\033[0;1;36mDEVSHELL ACTIVATED\033[0m\n"''
+    perSystem =
+      { lib, ... }:
+      {
+        config.devenv = {
+          modules = [
+            {
+              containers = lib.mkForce { }; # Workaround to remove containers from flake checks.
+            }
           ];
+          shells.default =
+            { pkgs, ... }:
+            {
+              enterShell = lib.concatStringsSep "\n" [
+                (lib.getExe pkgs.splash)
+                ''printf "\033[0;1;36mDEVSHELL ACTIVATED\033[0m\n"''
+              ];
 
-          enterTest = ''
-            nix --version
-          '';
+              enterTest = ''
+                nix --version
+              '';
 
-          git-hooks = {
-            default_stages = [ "pre-push" ];
-            hooks = {
-              actionlint.enable = true;
-              check-added-large-files = {
-                enable = true;
-                stages = [ "pre-commit" ];
+              git-hooks = {
+                default_stages = [ "pre-push" ];
+                hooks = {
+                  actionlint.enable = true;
+                  check-added-large-files = {
+                    enable = true;
+                    stages = [ "pre-commit" ];
+                  };
+                  check-json.enable = true;
+                  check-yaml.enable = true;
+                  deadnix.enable = true;
+                  detect-private-keys = {
+                    enable = true;
+                    stages = [ "pre-commit" ];
+                  };
+                  end-of-file-fixer.enable = true;
+                  flake-checker.enable = true;
+                  fmt = {
+                    enable = true;
+                    entry = "just fmt";
+                    name = "fmt";
+                    pass_filenames = false;
+                  };
+                  mixed-line-endings.enable = true;
+                  nil.enable = true;
+                  no-commit-to-branch = {
+                    enable = true;
+                    stages = [ "pre-commit" ];
+                  };
+                  ripsecrets = {
+                    enable = true;
+                    stages = [ "pre-commit" ];
+                  };
+                  shellcheck = {
+                    enable = true;
+                    excludes = [ ".envrc" ];
+                  };
+                  shfmt.enable = true;
+                  statix.enable = true;
+                };
               };
-              check-json.enable = true;
-              check-yaml.enable = true;
-              deadnix.enable = true;
-              detect-private-keys = {
-                enable = true;
-                stages = [ "pre-commit" ];
-              };
-              end-of-file-fixer.enable = true;
-              flake-checker.enable = true;
-              fmt = {
-                enable = true;
-                entry = "just fmt";
-                name = "fmt";
-                pass_filenames = false;
-              };
-              mixed-line-endings.enable = true;
-              nil.enable = true;
-              no-commit-to-branch = {
-                enable = true;
-                stages = [ "pre-commit" ];
-              };
-              ripsecrets = {
-                enable = true;
-                stages = [ "pre-commit" ];
-              };
-              shellcheck = {
-                enable = true;
-                excludes = [ ".envrc" ];
-              };
-              shfmt.enable = true;
-              statix.enable = true;
+
+              languages.nix.enable = true;
             };
-          };
-
-          languages.nix.enable = true;
         };
-    };
+      };
     touchup.attr.packages.any.attr = {
       # Remove deprecated packages that devenv includes.
       devenv-test.enable = false;
