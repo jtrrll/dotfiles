ba76f5696ef833b2ea4e41b98f75f57e931a1b05
jtrrll <jacksonterrill3@gmail.com>
2026-06-28 17:47:12 -0400

    nix conf

--- modules/github/ci.nix
+++ modules/github/ci.nix
@@ -10,6 +10,7 @@
       yaml = pkgs.formats.yaml { };
 
       nixInstallerConf = ''
+        allow-import-from-derivation = false
         extra-substituters = https://devenv.cachix.org https://install.determinate.systems https://nix-community.cachix.org
         extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
       '';
