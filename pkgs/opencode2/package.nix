8f6dd6c6fb1a64be8041397b5bfd499af86930b5
jtrrll <77407057+jtrrll@users.noreply.github.com>
2026-09-18 10:31:46 +0000

    deps(packages): update package dependencies

--- pkgs/opencode2/package.nix
+++ pkgs/opencode2/package.nix
@@ -23,14 +23,14 @@
 }:
 let
   nodeModulesHashes = {
-    aarch64-darwin = "sha256-wAea8+jajnMDxZ6XJL+Hsrf0621hwtBtWyD1+dS45dE=";
-    aarch64-linux = "sha256-Wc8OT2DRZpVo56KaoGE0Hsj1NDknakbWXO9w2qy6j+0=";
-    x86_64-linux = "sha256-U9IuP/ev6w4urvogOwQyl3rdumY6W4YaY18NkFaOVHU=";
+    aarch64-darwin = "wAea8+jajnMDxZ6XJL+Hsrf0621hwtBtWyD1+dS45dE=";
+    aarch64-linux = "Wc8OT2DRZpVo56KaoGE0Hsj1NDknakbWXO9w2qy6j+0=";
+    x86_64-linux = "U9IuP/ev6w4urvogOwQyl3rdumY6W4YaY18NkFaOVHU=";
   };
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "2.0.4";
+  version = "2.0.8";
 
   __structuredAttrs = true;
   strictDeps = true;
@@ -39,7 +39,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1SdZ+VFhhld8R0yYgHtfIgq8kL46xAnAJ4QrRfbKiOw=";
+    hash = "sha256-9sZ8akR8S4U7Ff0H+CbpGxbFu2U043aKCCx306TOUGs=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
--- pkgs/service_status/package.nix
+++ pkgs/service_status/package.nix
@@ -16,7 +16,7 @@ buildGoModule (finalAttrs: {
   pname = "service-status";
   version = "0.1.0";
   src = ./src;
-  vendorHash = "sha256-Ip2GuQDOolMyDvfmXcJRlY2rMp1amS8owkqcNMOR1+Y=";
+  vendorHash = "sha256-Sa3fqzEYRT6oU7Uw8FTCG2ZQDbe2xRcray/Js1iEz3s=";
 
   env.CGO_ENABLED = 0;
 
@@ -43,7 +43,7 @@ buildGoModule (finalAttrs: {
 
       (cd "$srcDir" && go get -u ./... && go mod tidy)
 
-      sed -i 's|vendorHash = "[^"]*"|vendorHash = ""|' "$pkgFile"
+      sed -i 's|vendorHash = "sha256-Sa3fqzEYRT6oU7Uw8FTCG2ZQDbe2xRcray/Js1iEz3s="]*"|vendorHash = ""|' "$pkgFile"
 
       hash=$(
         nix build "$root#service-status" --no-link 2>&1 \
